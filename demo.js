4a0e2bd6941badde8e34f2f9a543b9dc7d696566
RudrakshDev <wis40311@gmail.com>
2025-04-09 12:31:53 +0000

    Initial Commit

--- demo.js
+++ demo.js
@@ -1,10 +1,8 @@
-import express from 'express';
-
+const express = require('express');
 const app = express();
 
-// Add your routes
 app.get('/health', (req, res) => {
   res.send('OK');
 });
 
-export default app;
+module.exports = app;
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,5 @@
-import request from 'supertest';
-import app from '../demo.js'; // use demo.js here
+const request = require('supertest');
+const app = require('../demo');
 
 describe('GET /health', () => {
   it('should return OK', async () => {
